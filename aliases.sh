3ec66d6745c47b650a9647f46bb34c75a88cb418
Rafael Carvalho <rafael.c.carvalho@gmail.com>
2019-04-17 07:34:39 -0300

    Add alias to a formated git log

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
 alias docker-rm='docker rm -f $(docker ps -aq)'
 alias docker-rmi='docker rmi -f $(docker images -q)'
 alias docker-clear='docker-rm; docker-rmi'
+alias glog="git log --graph --pretty=format:'%C(yellow)%h%Creset -%Cred%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
