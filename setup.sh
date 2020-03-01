bb68b3322e98912f4f5c003fcd5d411b8e44a5f1
Rafael Carvalho <rafael.carvalho@locaweb.com.br>
2020-03-01 17:51:12 +0000

    Add keyboard configuration to setup

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ git config --global user.email "rafael.carvalho@locaweb.com.br"  && \
 git config --global core.editor vim                              && \
 git config --global merge.tool vimdiff                           && \
 
+ln -sf $CURRENT_DIR/inputrc ~/.inputrc     && \
 ln -sf $CURRENT_DIR/tmux.conf ~/.tmux.conf && \
 ln -sf -T $CURRENT_DIR/vim ~/.vim          && \
 git submodule init && git submodule update && \
