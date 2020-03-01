7dd15b317b3891340bae32502f17305c92140053
Rafael Carvalho <rafael.carvalho@locaweb.com.br>
2020-03-01 17:32:59 +0000

    Update setup script
    1. Make Locaweb email the default for git
    2. Source custom bash into .bashrc

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 CURRENT_DIR="$(pwd)"
 
-git config --global user.name "Rafael Carvalho"               && \
-git config --global user.email "rafael.c.carvalho@gmail.com"  && \
-git config --global core.editor vim                           && \
-git config --global merge.tool vimdiff                        && \
+git config --global user.name "Rafael Carvalho"                  && \
+git config --global user.email "rafael.carvalho@locaweb.com.br"  && \
+git config --global core.editor vim                              && \
+git config --global merge.tool vimdiff                           && \
 
 ln -sf $CURRENT_DIR/tmux.conf ~/.tmux.conf && \
-ln -sf -T $CURRENT_DIR/vim ~/.vim
+ln -sf -T $CURRENT_DIR/vim ~/.vim          && \
 git submodule init && git submodule update && \
-vim +PluginInstall +qall
+vim +PluginInstall +qall                   && \
+echo "source $CURRENT_DIR/bashrc" >> ~/.bashrc
+
 
