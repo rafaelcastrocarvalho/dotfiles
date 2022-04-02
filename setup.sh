d3ea0a6e93295d5a6ddc0b58e9898be7587b44f4
Rafael Carvalho <rafael.carvalho@locaweb.com.br>
2022-04-02 08:29:13 -0300

    Some more Ubuntu configs

--- setup.sh
+++ setup.sh
@@ -9,13 +9,14 @@ CURRENT_DIR="$(pwd)"
 
 ## Ubuntu packages
 #sudo add-apt-repository ppa:kelleyk/emacs                                       && \
-#sudo apt-get install gnome-tweak-tool xcape git vim tmux docker postgresql jq    \
-#                     zlib1g-dev build-essential libssl-dev libreadline-dev       \
-#                     libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev \
-#                     libffi-dev emacs26                                         && \
+#sudo apt-get install gnome-tweak-tool xcape git vim tmux docker emacs26            \
+#                     cmake libmysqlclient-dev libpq-dev libpq5 postgresql jq       \
+#                     zlib1g-dev build-essential libssl-dev libreadline-dev         \
+#                     libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev   \
+#                     libffi-dev silversearcher-ag                               && \
+#git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1           && \
 #git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf                  && \
 #~/.fzf/install                                                                  && \
-#git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1           && \
 
 ## CapsLock as Ctrl and Esc
 #setxkbmap -option 'caps:ctrl_modifier'                                          && \
