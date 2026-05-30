c1482a6b0227763cbc990c75a33b57210b3b1185
Rafael Carvalho <rafael.c.carvalho@gmail.com.br>
2026-05-30 12:02:41 -0300

    Update treesitter to use branch "main"
    It's the current stable branch - master is deprecated.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,9 +2,9 @@ return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
-    -- branch = 'main',
+    branch = 'main',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter.config', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = {
